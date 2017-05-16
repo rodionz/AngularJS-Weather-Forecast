9cb3bf879ce364fd8cb1c278efa67ec43a735688
Rodion Zinovyev <rodionz@012.net.il>
2017-05-16 19:27:39 +0300

    modeles, controllers

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+
+
 var weatherApp = angular.module('weatherApp', []);
+
+weatherApp.value('ngRoute', 'ngRoute');
+
+weatherApp.value('ngResource', 'ngResource');
+
+
+weatherApp.controller('homeController', ['$scope', function ($scope) { }]);
