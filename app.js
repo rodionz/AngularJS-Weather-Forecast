7d9d467fc9f89c2f36e779cd0ed2bace8b1421b5
Rodion Zinovyev <rodionz@012.net.il>
2017-05-17 20:16:54 +0300

    forecat

--- app.js
+++ app.js
@@ -27,21 +27,25 @@ $scope.$watch('city',function(){
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope','$resource','cityService', function ($scope,$resource,cityService)
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams,cityService)
  { 
 
     $scope.city = cityService.city; 
 
+    $scope.days = $routeParams.days || 2;
 
     $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/");
 
 
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, units : "metric", appid: "2d794d499eac9db4bee944fe95e97a0a" }, function () {
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days, units : "metric", appid: "2d794d499eac9db4bee944fe95e97a0a" }, function () {
 
         console.log($scope.weatherResult);
     });
 
-   
+
+    $scope.converttoDate = function (dt) {
+        return new Date(dt * 1000);
+    }
 
  }]);
 
@@ -60,7 +64,11 @@ weatherApp.config(function ($routeProvider) {
             controller: 'forecastController'
 
         })
+        .when('/forecast/:days', {
+            templateUrl: 'pages/forecast.html',
+            controller: 'forecastController'
 
+        })
 
 });
 
