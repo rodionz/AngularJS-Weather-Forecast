3e2a4fb820ba7cfd96b312957f7f7518a2a89786
Rodion Zinovyev <rodionz@012.net.il>
2017-05-18 17:10:58 +0300

    directive

--- app.js
+++ app.js
@@ -51,6 +51,26 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
 
 
 
+weatherApp.directive("weatherReport", function () {
+
+    return {
+        restrict: "E",
+        templateUrl: "directives/weatherReport.html",
+        replace: true,
+        scope: {
+            weatherDay = "=",
+            convertDate = "&",
+            dateFormat = "@"
+
+        }
+
+
+    }
+
+
+
+});
+
 weatherApp.config(function ($routeProvider) {
 
     $routeProvider
